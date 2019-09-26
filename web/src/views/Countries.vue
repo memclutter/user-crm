31c0634f098d966ecde07664e509ca0c74b2ca53
Memory Clutter <memclutter@gmail.com>
2019-09-26 22:06:11 +0300

    Countries list

--- web/src/views/Countries.vue
+++ web/src/views/Countries.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    Countries
-  </div>
+  <v-list three-line>
+    <v-list-item v-for="item in items" :key="item.id" @click="">
+      <v-list-item-action>
+        <v-list-item-icon >
+          <flag :iso="item.code" :squared="false"/>
+        </v-list-item-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{item.name}}
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
 </template>
+
+<script>
+import $http from '@/http'
+
+export default {
+
+  data: () => ({
+    loading: false,
+    offset: 0,
+    limit: 25,
+    items: [],
+    totalCount: 0
+  }),
+
+  async mounted() {
+    await this.load();
+  },
+
+  methods: {
+    async load() {
+      this.loading = true;
+
+      try {
+        const params = {limit: this.limit, offset: this.offset};
+        const {data: {totalCount, items}} = await $http.get('/countries', {params});
+
+        this.totalCount = totalCount;
+        this.items = items;
+      } finally {
+        this.loading = false;
+      }
+    }
+  }
+}
+</script>

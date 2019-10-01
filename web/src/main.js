240b92742104eddaabcfc81063b14395cb261e1b
Memory Clutter <memclutter@gmail.com>
2019-10-01 23:19:01 +0300

    Install vue moment

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import './plugins/flags'
 import './plugins/axios'
+import './plugins/moment'
 
 Vue.config.productionTip = false
 

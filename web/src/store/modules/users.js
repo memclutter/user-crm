0f5011065647c25d328ceaae3d170e1110100850
Memory Clutter <memclutter@gmail.com>
2019-10-04 21:40:35 +0300

    Copy paste users store module from countries module

--- web/src/store/modules/users.js
+++ web/src/store/modules/users.js
@@ -1,3 +1,5 @@
+import { getField, updateField} from 'vuex-map-fields'
+
 import axios from '@/plugins/axios'
 
 export default {
@@ -9,21 +11,71 @@ export default {
     offset: 0,
     limit: 10,
     items: [],
-    totalCount: 0
+    totalCount: 0,
+    createDialog: false,
+    updateDialog: false,
+    removeDialog: false,
+    form: {
+      loading: false,
+      pk: null,
+      fields: {
+        username: null,
+        email: null,
+        avatarUrl: null,
+        gender: null,
+        birthday: null,
+        countryCode: null
+      },
+      errors: {}
+    }
   },
 
   getters: {
+    getField,
     page: state => state.offset / state.limit + 1,
     pageCount: state => Math.floor(state.totalCount / state.limit) + (state.totalCount % state.limit > 0 ? 1 : 0),
   },
 
   mutations: {
+    updateField,
     setLoading: (state, loading) => state.loading = loading,
     setOffset: (state, offset) => state.offset = offset,
     setData: (state, {items, totalCount}) => {
       state.items = items;
       state.totalCount = totalCount;
-    }
+    },
+    openCreateDialog: (state) => state.createDialog = true,
+    closeCreateDialog: (state) => state.createDialog = false,
+    openUpdateDialog: (state, item) => {
+      state.updateDialog = true;
+      state.form.pk = item.code;
+      state.form.fields = item;
+    },
+    closeUpdateDialog: (state) => {
+      state.updateDialog = false;
+      state.form.pk = null;
+      state.form.fields = {
+        code: null,
+        name: null
+      };
+      state.form.errors = {};
+    },
+    openRemoveDialog: (state, item) => {
+      state.removeDialog = true;
+      state.form.pk = item.code;
+      state.form.fields = item;
+    },
+    closeRemoveDialog: (state) => {
+      state.items = state.items.filter(item => item.code !== state.form.pk);
+      state.removeDialog = false;
+      state.form.pk = null;
+      state.form.fields = {
+        code: null,
+        name: null
+      };
+    },
+    setFormLoading: (state, loading) => state.form.loading = loading,
+    setFormErrors: (state, errors) => state.form.errors = errors
   },
 
   actions: {
@@ -51,5 +103,47 @@ export default {
 
       await context.dispatch('load');
     },
+
+    async remove(context) {
+      context.commit('setFormLoading', true);
+
+      try {
+        const {status} = await axios.delete(`/users/${context.state.form.pk}`);
+
+        if (status === 204) {
+          context.commit('closeRemoveDialog');
+        } else {
+          // TODO handle other statuses
+        }
+      } finally {
+        context.commit('setFormLoading', false);
+      }
+    },
+
+    async submit(context, update) {
+      context.commit('setFormLoading', true);
+
+      try {
+        if (update) {
+          const {status, data} = await axios.put(`/users/${context.state.form.pk}`, context.state.form.fields);
+
+          if (status === 200) {
+            context.commit('closeUpdateDialog');
+          } else {
+            context.commit('setFormErrors', data);
+          }
+        } else {
+          const {status, data} = await axios.post('/users', context.state.form.fields);
+
+          if (status === 201) {
+            context.commit('closeCreateDialog');
+          } else {
+            context.commit('setFormErrors', data);
+          }
+        }
+      } finally {
+        context.commit('setFormLoading', false);
+      }
+    }
   }
 }

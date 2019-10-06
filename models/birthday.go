c4e98c6771b176c0d123e59779f77835d231cad7
Memory Clutter <memclutter@gmail.com>
2019-10-06 12:47:20 +0300

    Implement scanner and valuer interface for birthday type

--- models/birthday.go
+++ models/birthday.go
@@ -1,11 +1,26 @@
 package models
 
 import (
+	"database/sql/driver"
+	"errors"
 	"fmt"
 	"time"
 )
 
 type Birthday time.Time
+
+func (b Birthday) Value() (driver.Value, error) {
+	return time.Time(b), nil
+}
+
+func (b *Birthday) Scan(src interface{}) error {
+	t, ok := src.(time.Time)
+	if !ok {
+		return errors.New("invalid database value for birthday type")
+	}
+	*b = Birthday(t)
+	return nil
+}
 
 func (b *Birthday) UnmarshalJSON(data []byte) error {
 	t, err := time.Parse(`"2006-01-02"`, string(data))

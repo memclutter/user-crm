9817fe934b522e29caa89e48067becf2be39585e
Memory Clutter <memclutter@gmail.com>
2019-10-06 13:23:20 +0300

    Fix base dialog

--- web/src/components/BaseDialog.vue
+++ web/src/components/BaseDialog.vue
@@ -1,14 +1,14 @@
 <template>
   <v-dialog :value="value" @input="$emit('input', $event)" :width="width" :max-width="maxWidth">
     <v-card>
-      <v-toolbar dark :color="color">
+      <v-toolbar dark :color="color" flat>
         <v-toolbar-title>{{title}}</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn icon @click="$emit('input', false)">
           <v-icon>fa-times</v-icon>
         </v-btn>
       </v-toolbar>
-      <v-card-text>
+      <v-card-text class="pt-8">
         <slot></slot>
       </v-card-text>
     </v-card>
